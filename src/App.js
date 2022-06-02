5b4a62164508d96228701f5e4e27617dde9def46
atlas <a.sproson@outlook.com>
2022-06-02 09:34:51 +0100

    Removes reference to App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <NavBar />
     </div>
   );
 }
