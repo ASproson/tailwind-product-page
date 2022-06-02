4c8c82196fb9fdc7c4001ff5a726f62f9e6650ed
atlas <a.sproson@outlook.com>
2022-06-02 15:03:40 +0100

    Mostly completes NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+
+    let style = {divClass: 'block hamburger md:hidden focus:outline-none'}
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <div>
             {/* Navbar */}
@@ -24,6 +29,23 @@ const NavBar = () => {
                     </div>
                     {/* Button */}
                     <Link to='/' className='hidden lg:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'>Get Started</Link>
+
+                    {/* Hamburger Icon */}
+                    <button id="menu-btn" className={`${style.divClass}}`} onClick={() => setIsMenuOpen((prev) => !prev)}>
+                        <span className="hamburger-top"></span>
+                        <span className="hamburger-middle"></span>
+                        <span className="hamburger-bottom"></span>
+                    </button>
+                </div>
+                {/* Mobile Menu */}
+                <div className="md:hidden">
+                    <div id="menu" className="absolute flex hidden flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
+                        <Link to='/' className='hover:text-darkGrayishBlue'>Pricing</Link>
+                        <Link to='/' className='hover:text-darkGrayishBlue'>Product</Link>
+                        <Link to='/' className='hover:text-darkGrayishBlue'>About Us</Link>
+                        <Link to='/' className='hover:text-darkGrayishBlue'>Careers</Link>
+                        <Link to='/' className='hover:text-darkGrayishBlue'>Contact</Link>
+                    </div>
                 </div>
             </nav>
         </div>
